7e09aa013316079add851f212fcecdc713689e5a
brahianf <brahianf@unicauca.edu.co>
2020-08-06 22:04:52 -0500

    React Hooks: useEffect y useState

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import '../assets/styles/Home.scss'
 import Header from '../components/Header.jsx'
@@ -8,28 +8,40 @@ import Carousel from '../components/Carousel.jsx'
 import CarouselItem from '../components/CarouselItem.jsx'
 import Footer from '../components/Footer.jsx'
 
-const Home = () => (
-    <div className="Home">
-        <Header />
-        <Search />
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem /> 
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories  title="Tendencias">
-            <Carousel>
-                <CarouselItem /> 
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-);
+const Home = () => {
+    const [ videos, setVideos ] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [])
+
+    console.log(videos)
+
+    return(
+        <div className="Home">
+            <Header />
+            <Search />
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItem /> 
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories  title="Tendencias">
+                <Carousel>
+                    <CarouselItem /> 
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    )
+};
 
 export default Home;
