19dfa402f6bf4cceb42af360115cf1ec0214961e
brahianf <brahianf@unicauca.edu.co>
2020-08-07 00:32:59 -0500

    API

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 import playIcon from '../assets/static/play-icon.png'
 import plusIcon from '../assets/static/plus-icon.png'
 
-const CarouselItem =  () => (
+const CarouselItem =  ({cover, title, year, contentRating, duration}) => (
     <div className="carousel__item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/4348226/pexels-photo-4348226.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+        <img className="carousel-item__img" src={cover} alt={title}/>
         <div className="carousel-item__details">
             <div>
                 <img src={playIcon}/>
                 <img src={plusIcon}/>
             </div>
-            <p className="carousel-item__details--title">Título Desc</p>
-            <p className="carousel-item__details--subtitle">2019 16+</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
         </div>
     </div>
 )
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -9,34 +9,33 @@ import CarouselItem from '../components/CarouselItem.jsx'
 import Footer from '../components/Footer.jsx'
 
 const Home = () => {
-    const [ videos, setVideos ] = useState([])
-
+    const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] });
     useEffect(() => {
         fetch('http://localhost:3000/initalState')
             .then(response => response.json())
             .then(data => setVideos(data))
     }, [])
 
-    console.log(videos)
+    console.log(videos.trends)
 
     return(
         <div className="Home">
             <Header />
             <Search />
-            <Categories title="Mi Lista">
-                <Carousel>
-                    <CarouselItem /> 
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
+            {videos.mylist.length > 0 && (
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        <CarouselItem /> 
+                    </Carousel>
+                </Categories>
+            )}
+
             <Categories  title="Tendencias">
                 <Carousel>
-                    <CarouselItem /> 
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
+                    {videos.trends.map( item => 
+                        // console.log(item);
+                        <CarouselItem key={item.id} {...item}/> 
+                    )}
                 </Carousel>
             </Categories>
             <Footer />
